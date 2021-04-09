cf5c352656655037f8a80b2f32416fb1c1586be4
CK-140 <chitwan.kasana@gmail.com>
2021-04-09 13:14:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashreadyfit/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.splashreadyfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_SplashReadyFit);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
